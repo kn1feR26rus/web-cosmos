44786aea00ea2a65e8dc20f19b635a2ea37fc7e3
kn1feR26rus <kn1feR26rus@yandex.ru>
2023-05-30 14:28:58 +0300

    problems

--- js/index.js
+++ js/index.js
@@ -12,8 +12,17 @@ $(document).ready(function() {
         var title2_et = $('.title2_hr').offset().top;
         var title2_eh = $('.title2_hr').outerHeight(); 
 
-        var title3_et = $('.title2_hr').offset().top;
-        var title3_eh = $('.title2_hr').outerHeight(); 
+        var title3_et = $('.title3_hr').offset().top;
+        var title3_eh = $('.title3_hr').outerHeight(); 
+
+        var title4_et = $('.title4_hr').offset().top;
+        var title4_eh = $('.title4_hr').outerHeight(); 
+
+        var title5_et = $('.title5_hr').offset().top;
+        var title5_eh = $('.title5_hr').outerHeight(); 
+
+        var title6_et = $('.title6_hr').offset().top;
+        var title6_eh = $('.title6_hr').outerHeight();
 
         if (wt + wh >= title1_et || wh + wt == dh || title1_eh + title1_et < wh){
             $('.title1_hr').addClass('active')
@@ -23,6 +32,15 @@ $(document).ready(function() {
         }
         if (wt + wh >= title3_et || wh + wt == dh || title3_eh + title3_et < wh){
             $('.title3_hr').addClass('active')
+        }
+        if (wt + wh >= title4_et || wh + wt == dh || title4_eh + title4_et < wh){
+            $('.title4_hr').addClass('active')
+        }
+        if (wt + wh >= title5_et || wh + wt == dh || title5_eh + title5_et < wh){
+            $('.title5_hr').addClass('active')
+        }
+        if (wt + wh >= title6_et || wh + wt == dh || title6_eh + title6_et < wh){
+            $('.title6_hr').addClass('active')
         }
     });
 })
