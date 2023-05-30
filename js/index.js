956340c3af9da15827936467eb2f87754dbe37b2
kn1feR26rus <kn1feR26rus@yandex.ru>
2023-05-30 12:40:41 +0300

    company

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    
+    AOS.init();
+
+    $(window).scroll(function(){
+        var wt = $(window).scrollTop();
+        var wh = $(window).height();
+        var dh = $(document).height();  
+
+        var title1_et = $('.title1_hr').offset().top;
+        var title1_eh = $('.title1_hr').outerHeight(); 
+
+        var title2_et = $('.title2_hr').offset().top;
+        var title2_eh = $('.title2_hr').outerHeight(); 
+
+        if (wt + wh >= title1_et || wh + wt == dh || title1_eh + title1_et < wh){
+            $('.title1_hr').addClass('active')
+        }
+        if (wt + wh >= title2_et || wh + wt == dh || title2_eh + title2_et < wh){
+            $('.title2_hr').addClass('active')
+        }
+    });
 })
