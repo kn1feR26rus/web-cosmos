05a215bdcf7a90d8b7c4f97cebd8a09a1921203b
kn1feR26rus <kn1feR26rus@yandex.ru>
2023-05-30 13:09:22 +0300

    develop

--- js/index.js
+++ js/index.js
@@ -12,11 +12,17 @@ $(document).ready(function() {
         var title2_et = $('.title2_hr').offset().top;
         var title2_eh = $('.title2_hr').outerHeight(); 
 
+        var title3_et = $('.title2_hr').offset().top;
+        var title3_eh = $('.title2_hr').outerHeight(); 
+
         if (wt + wh >= title1_et || wh + wt == dh || title1_eh + title1_et < wh){
             $('.title1_hr').addClass('active')
         }
         if (wt + wh >= title2_et || wh + wt == dh || title2_eh + title2_et < wh){
             $('.title2_hr').addClass('active')
+        }
+        if (wt + wh >= title3_et || wh + wt == dh || title3_eh + title3_et < wh){
+            $('.title3_hr').addClass('active')
         }
     });
 })
